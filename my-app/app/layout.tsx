4f14cb7e0eb343a31827f654d17de8bdbc6c1afa
MaxwelJAMES29 <jamesmaxwelchrist@gmail.com>
2025-08-22 12:59:17 +0100

    yoyo

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono,IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({variable: "--font-geist-sans",subsets: ["latin"],});
+const ibmPLexSerif =IBM_Plex_Serif({ subsets:["latin"],
+   variable:"--font-ibm-plex-serif",weight:["400","700"]})
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],})
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for everyone.",
+  icons:{ icon:"/icons/logo.svg"}};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+  className={`${geistSans.variable} ${geistMono.variable} ${ibmPLexSerif.variable}`}
+      >{children}
+      </body>
+    </html>
+  );
+}
